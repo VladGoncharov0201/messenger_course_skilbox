d46e138275c8d6d149bfa5c0aa03daf786797436
VladGoncharov0201 <49080251+VladGoncharov0201@users.noreply.github.com>
2020-10-01 21:49:15 +0300

    Update server

--- server.py
+++ server.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello World! <br><a href='/status'>Статус</a>"
+
+
+@app.route("/status")
+def status():
+    return "Hello World!"
 
 
 app.run()
